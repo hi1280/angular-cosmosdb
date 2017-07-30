810961f0c23a3d88c0c26c9fa136b19cfd7fb456
hi1280 <hi12801280@gmail.com>
2017-07-30 21:43:33 +0900

    fix for azure app service

--- src/server/index.js
+++ src/server/index.js
@@ -3,15 +3,15 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
-const root = './';
+const root = __dirname + '/';
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(root, 'dist')));
+app.use(express.static(root));
 app.use('/api', routes);
 app.get('*', (req, res) => {
-  res.sendFile('dist/index.html', {root: root});
+  res.sendFile('index.html', {root: root});
 });
 
 const port = process.env.PORT || '3000';
