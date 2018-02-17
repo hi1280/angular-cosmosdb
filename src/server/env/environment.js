4c76087f9fd7028b4ae0873a58440260c1b234f8
hi1280 <hi12801280@gmail.com>
2018-02-17 17:30:48 +0900

    fix for azure app service on linux

---
+++ src/server/env/environment.js
@@ -0,0 +1,9 @@
+const cosmosPort = 10255; // replace with your port
+const dbName = 'my-cosmos-heroes-example';
+const key = 'lMz56Q5Ll1JctYwLEJNJxISvMBTd1ye0WtRfie6Dhlqa2QNLDB3mFfrYH1KRC1jxQaMOeXd4TvOhTIsXtUAvag==';
+
+module.exports = {
+  dbName,
+  key,
+  cosmosPort
+};
